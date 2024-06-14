2a243767f82453afd2317047776d807dd354abd1
Antonio Diaz Tula <antonio.diaz@drtis.com.br>
2024-06-14 19:09:15 -0300

    Atualizando anonuimizador

--- build/dcmjs.js
+++ build/dcmjs.js
@@ -17525,7 +17525,18 @@ b"+i+"*=d\
 	// "SoftwareVersion",
 	"ProtocolName", "AcquisitionDeviceProcessingDescription", "AcquisitionComments", "DetectorID", "AcquisitionProtocolDescription", "ContributionDescription", "ModifyingDeviceID", "ModifyingDeviceManufacturer", "ModifiedImageDescription", "ImageComments", "ImagePresentationComments", "StudyIDIssuer", "ScheduledStudyLocation", "ScheduledStudyLocationAET", "ReasonforStudy", "RequestingPhysician", "RequestingService", "RequestedProcedureDescription", "RequestedContrastAgent", "StudyComments", "AdmissionID", "IssuerOfAdmissionID", "ScheduledPatientInstitutionResidence", "AdmittingDate", "AdmittingTime", "DischargeDiagnosisDescription", "SpecialNeeds", "ServiceEpisodeID", "IssuerOfServiceEpisodeId", "ServiceEpisodeDescription", "CurrentPatientLocation", "PatientInstitutionResidence", "PatientState", "ReferencedPatientAliasSeq", "VisitComments", "ScheduledStationAET", "ScheduledPerformingPhysicianName", "SPSDescription", "ScheduledStationName", "SPSLocation", "PreMedication", "PerformedStationAET", "PerformedStationName", "PerformedLocation", "PerformedStationNameCodeSeq", "PPSID", "PPSDescription", "RequestAttributesSeq", "PPSComments", "AcquisitionContextSeq", "PatientTransportArrangements", "RequestedProcedureLocation", "NamesOfIntendedRecipientsOfResults", "IntendedRecipientsOfResultsIDSequence", "PersonAddress", "PersonTelephoneNumbers", "RequestedProcedureComments", "ReasonForTheImagingServiceRequest", "OrderEnteredBy", "OrderEntererLocation", "OrderCallbackPhoneNumber", "ImagingServiceRequestComments", "ConfidentialityPatientData", "ScheduledStationNameCodeSeq", "ScheduledStationGeographicLocCodeSeq", "PerformedStationGeoLocCodeSeq", "ScheduledHumanPerformersSeq", "ActualHumanPerformersSequence", "HumanPerformersOrganization", "HumanPerformersName", "VerifyingOrganization", "VerifyingObserverName", "AuthorObserverSequence", "ParticipantSequence", "CustodialOrganizationSeq", "VerifyingObserverIdentificationCodeSeq", "PersonName", "ContentSeq", "OverlayData", "OverlayComments", "IconImageSequence", "TopicSubject", "TopicAuthor", "TopicKeyWords", "TextString", "Arbitrary", "TextComments", "ResultsIDIssuer", "InterpretationRecorder", "InterpretationTranscriber", "InterpretationText", "InterpretationAuthor", "InterpretationApproverSequence", "PhysicianApprovingInterpretation", "InterpretationDiagnosisDescription", "ResultsDistributionListSeq", "DistributionName", "DistributionAddress", "InterpretationIdIssuer", "Impressions", "ResultComments", "DigitalSignaturesSeq", "DataSetTrailingPadding"];
 	function cleanTags(dict) {
-	  tagNamesToEmpty.forEach(function (tag) {
+	  var tagNamesToReplace = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : undefined;
+	  var customTagNamesToEmpty = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : undefined;
+
+	  if (tagNamesToReplace == undefined) {
+	    tagNamesToReplace = {
+	      "00100010": "ANON^PATIENT",
+	      "00100020": "ANON^ID"
+	    };
+	  }
+
+	  var tags = customTagNamesToEmpty != undefined ? customTagNamesToEmpty : tagNamesToEmpty;
+	  tags.forEach(function (tag) {
 	    var tagInfo = DicomMetaDictionary.nameMap[tag];
 
 	    if (tagInfo && tagInfo.version != "PrivateTag") {
@@ -17533,13 +17544,10 @@ b"+i+"*=d\
 	          tagString = Tag.fromPString(tagNumber).toCleanString();
 
 	      if (dict[tagString]) {
-	        log.log("empty tag " + tag);
 	        var newValue;
 
-	        if (tagString == "00100010") {
-	          newValue = ["ANON^PATIENT"];
-	        } else if (tagString == "00100020") {
-	          newValue = ["ANONID"];
+	        if (tagString in tagNamesToReplace) {
+	          newValue = [tagNamesToReplace[tagString]];
 	        } else {
 	          newValue = [];
 	        }
